b70faad0baba488b9499e442bfeed5bfeb6ea833
Kevin Suen <kevinsuen11@gmail.com>
2019-08-12 20:06:43 -0400

    add conditional styling on char length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $('.text-input').keyup(function() {
     $(this).siblings('.counter').text(140 - this.value.length);
+    if (this.value.length > 140) {
+      console.log('hi');
+      $(this).siblings('.counter').css({"color": "rgb(175, 0, 0)"});
+    } else {
+      $(this).siblings('.counter').css({"color": "#545149"})
+    }
   });
 });
