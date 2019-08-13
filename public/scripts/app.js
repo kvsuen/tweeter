ef819f5708e4456d710bd9ada401964a248b7a80
Kevin Suen <kevinsuen11@gmail.com>
2019-08-13 15:11:32 -0400

    add fetch tweet from ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       let $tweet = createTweetElement(tweet);
@@ -56,8 +31,18 @@ $(document).ready(function () {
     
     return $('<article>').addClass('tweet').html(markup);
   };
-  
-  renderTweets(data);
+
+  const loadTweets = function() {
+    $.ajax({
+      type: 'GET',
+      url: '/tweets'
+    })
+      .success(function(data) {
+        renderTweets(data);
+      });
+  };
+
+  loadTweets();
 
   $('.new-tweet').submit(function(event) {
     event.preventDefault();
