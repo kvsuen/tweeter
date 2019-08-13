3cabefcd6244dfbd56892301ff384e665dc69318
Kevin Suen <kevinsuen11@gmail.com>
2019-08-13 11:00:53 -0400

    eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       console.log('hi');
       $(this).siblings('.counter').css({"color": "rgb(175, 0, 0)"});
     } else {
-      $(this).siblings('.counter').css({"color": "#545149"})
+      $(this).siblings('.counter').css({"color": "#545149"});
     }
   });
 });
