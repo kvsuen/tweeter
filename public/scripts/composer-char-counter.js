42b5be37eb32dd91ca9e7bd36ff26b9149e5496b
Kevin Suen <kevinsuen11@gmail.com>
2019-08-14 16:37:12 -0400

    remove console log

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('.text-input').keyup(function() {
     $(this).siblings('.counter').text(140 - this.value.length);
     if (this.value.length > 140) {
-      console.log('hi');
       $(this).siblings('.counter').css({"color": "rgb(175, 0, 0)"});
     } else {
       $(this).siblings('.counter').css({"color": "#545149"});
