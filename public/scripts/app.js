54b4e44840773b3b8c63f9f5c978df6898a4ce32
Kevin Suen <kevinsuen11@gmail.com>
2019-08-14 16:37:02 -0400

    modularize code, update icons, update validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,74 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const getTimeSincePost = function(initialTime) {
-  const timeInSeconds = (Date.now() - initialTime) / 1000;
-  const timeInMinutes = timeInSeconds / 60;
-  const timeInHours = timeInMinutes / 60;
-  const timeInDays = timeInHours / 24;
-
-  if (timeInSeconds < 60) {
-    return `${Math.floor(timeInSeconds)} seconds ago`;
-  } else if (timeInMinutes < 60) {
-    return `${Math.floor(timeInMinutes)} minutes ago`;
-  } else if (timeInHours < 24) {
-    return `${Math.floor(timeInHours)} hours ago`;
-  } else {
-    return `${Math.floor(timeInDays)} days ago`;
+const renderTweets = function(tweets) {
+  const container = $('#tweets-container');
+  for (const tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    container.append($tweet);
   }
 };
 
-$(document).ready(function () {
-  const renderTweets = function(tweets) {
-    const container = $('#tweets-container');
-    for (const tweet of tweets) {
-      let $tweet = createTweetElement(tweet);
-      container.append($tweet);
-    }
-  };
-  
-  const createTweetElement = function(tweet) {
-    const markup = `
-        <header>
-          <img class="avatar" src=${tweet.user.avatars}>
-          <p class="user-name">${tweet.user.name}</p>
-          <h5 class="handle">${tweet.user.handle}</h5>
-        </header>
-        <div class="tweet-body">
-          <p>${tweet.content.text}</p>
+const createTweetElement = function(tweet) {
+  const markup = `
+      <header>
+        <img class="avatar" src=${tweet.user.avatars} alt="User Avatar" title="User Avatar" >
+        <p class="user-name">${tweet.user.name}</p>
+        <h5 class="handle">${tweet.user.handle}</h5>
+      </header>
+      <div class="tweet-body">
+        <p>${tweet.content.text}</p>
+      </div>
+      <footer>
+        <p class="date">${getTimeSincePost(tweet.created_at)}</p>
+        <div class="tweet-user-options">
+          <button><img src="/images/flag.png" alt="User Avatar" title="User Avatar"> </button> 
+          <button><img src="/images/retweet.png" alt="User Avatar" title="User Avatar"> </button>  
+          <button><img src="/images/rose.png" alt="User Avatar" title="User Avatar"> </button>
         </div>
-        <footer>
-          <p class="date">${getTimeSincePost(tweet.created_at)}</p>
-          <div class="tweet-user-options">
-            <button>🚩</button> 
-            <button>🌀</button>  
-            <button>❤</button>
-          </div>
-        </footer>
-    `;
-    
-    return $('<article>').addClass('tweet').html(markup);
-  };
+      </footer>
+  `;
+  
+  return $('<article>').addClass('tweet').html(markup);
+};
 
-  const loadTweets = function() {
-    $.ajax({
-      type: 'GET',
-      url: '/tweets'
-    })
-      .success(function(data) {
-        renderTweets(data);
-      });
-  };
 
+
+const loadTweets = function() {
+  $.ajax({
+    type: 'GET',
+    url: '/tweets'
+  })
+    .success(function(data) {
+      renderTweets(data);
+    });
+};
+
+$(document).ready(function () {
   loadTweets();
 
   $('.new-tweet').submit(function(event) {
     event.preventDefault();
-    
-    //server expects query string
     const form = $(this).children('form');
-    const formData = form.serialize();
 
+    // userInput used for tweet validation prior to sanitizing
+    const userInput = $('.new-tweet textarea').val()
+    
+    // serialize because server expects query string
+    // sanitizing user input
     const safeFormData = {};
     $.each(form.serializeArray(), function() {
       safeFormData[this.name] = this.value
@@ -83,10 +70,17 @@ $(document).ready(function () {
         .replace(/\//g, '&#x2F');
     });
 
-    if (formData.length === 5) {
-      alert('Empty tweet. Please enter a message!');
-    } else if ( formData.length > 145) {
-      alert('Tweet is too long!');
+    $('.error-no-msg').fadeOut();
+    $('.error-excess-msg').fadeOut();
+    
+    if (userInput.length === 0) {
+      $('.error-no-msg').slideDown(400, function() {
+        $('.new-tweet').find('.text-input').focus();
+      });
+    } else if (userInput.length > 140) {
+      $('.error-excess-msg').slideDown(400, function() {
+        $('.new-tweet').find('.text-input').focus();
+      });
     } else {
       $.ajax({
         type: 'POST',
@@ -94,7 +88,7 @@ $(document).ready(function () {
         data: safeFormData,
       })
         .success(function() {
-          $('.new-tweet').find('textarea').val('');
+          $('.new-tweet').find('.text-input').val('');
           $('.new-tweet').find('.counter').text(140);
           // update page when using posts tweet
           // do another ajax request to get updated tweet data and prepend new tweet to the page
@@ -104,26 +98,7 @@ $(document).ready(function () {
             url: '/tweets'
           })
             .success(function(data) {
-              $("#tweets-container").prepend($('<article>').addClass('tweet').html(
-                `
-                <header>
-                  <img class="avatar" src=${data[0].user.avatars}>
-                  <p class="user-name">${data[0].user.name}</p>
-                  <h5 class="handle">${data[0].user.handle}</h5>
-                </header>
-                <div class="tweet-body">
-                  <p>${data[0].content.text}</p>
-                </div>
-                <footer>
-                  <p class="date">${getTimeSincePost(data[0].created_at)}</p>
-                  <div class="tweet-user-options">
-                    <button>🚩</button> 
-                    <button>🌀</button>  
-                    <button>❤</button>
-                  </div>
-                </footer>
-                `
-              ));
+              $("#tweets-container").prepend(createTweetElement(data[0]));
             });
         });
     }
@@ -131,7 +106,7 @@ $(document).ready(function () {
 
   $('nav .write-tweet-toggle').click(function() {
     $('.new-tweet').slideToggle(400, function() {
-      $('.new-tweet').find('textarea').focus();
+      $('.new-tweet').find('.text-input').focus();
     });
   });
 
